b49d4050b52570a3da98cd969fe2434b938c1314
GOWREESWARI23 <gowrisubramanianfire@gmail.com>
2016-07-28 12:16:33 +0530

    Update Gow.java

--- Gow.java
+++ Gow.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.scanner;
 class Gow
 {
-int i,a,fact;
+int i,a,fact=1;
 Scanner in=new Scanner(System.in);
 System.out.println("enter the value for a");
 a=in.nextint();
 for(i=1;i<=n;i++)
 {
-fact=fact*1;
+fact=fact*i;
 System.out.println("the factorial of a is +fact");
 }
 }
