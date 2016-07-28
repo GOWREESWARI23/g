19e578661dbf2d06328e29810d75b7ca3458df95
GOWREESWARI23 <gowrisubramanianfire@gmail.com>
2016-07-28 12:11:04 +0530

    Create Gow.java

---
+++ Gow.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.scanner;
+class Gow
+{
+int i,a,fact;
+Scanner in=new Scanner(System.in);
+System.out.println("enter the value for a");
+a=in.nextint();
+for(i=1;i<=n;i++)
+{
+fact=fact*1;
+System.out.println("the factorial of a is +fact");
+}
+}
